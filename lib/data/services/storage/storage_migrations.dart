423fdc0e44b8e567efb2e767507f15129b016374
Yannis <yannis.b8@gmail.com>
2021-10-25 19:41:24 +0200

    fix loading error web

--- lib/data/services/storage/storage_migrations.dart
+++ lib/data/services/storage/storage_migrations.dart
@@ -15,9 +15,7 @@ class StorageMigrations {
   late SharedPreferences prefs;
   constructor() {}
   run() async {
-    var root = await getApplicationDocumentsDirectory();
     prefs = await SharedPreferences.getInstance();
-    rootPath = root.path;
     await removeOldManifest();
     await updateAccountInfo();
   }
@@ -29,10 +27,6 @@ class StorageMigrations {
   updateAccountInfo() async {
     var latestMembership = prefs.getString("latestMembership");
     var latestToken = prefs.getString("latestToken");
-    var cachedLoadoutsFile = File("$rootPath/cached_loadouts.json");
-    var trackedObjectivesFile = File("$rootPath/tracked_objectives.json");
-    var cachedProfileFile = File("$rootPath/cached_profile.json");
-    var cachedRaidHashesFile = File("$rootPath/cached_raid_hashes.json");
     if (latestMembership == null) return;
     try {
       var membershipJson = jsonDecode(latestMembership);
@@ -62,32 +56,8 @@ class StorageMigrations {
       membershipStorage.setString(
           StorageKeys.membershipUUID, prefs.getString("littlelight_device_id"));
 
-      if (await cachedLoadoutsFile.exists()) {
-        var str = await cachedLoadoutsFile.readAsString();
-        var json = jsonDecode(str);
-        membershipStorage.setJson(StorageKeys.cachedLoadouts, json);
-        await cachedLoadoutsFile.delete();
-      }
-
-      if (await trackedObjectivesFile.exists()) {
-        var str = await trackedObjectivesFile.readAsString();
-        var json = jsonDecode(str);
-        membershipStorage.setJson(StorageKeys.trackedObjectives, json);
-        await trackedObjectivesFile.delete();
-      }
-
-      if (await cachedRaidHashesFile.exists()) {
-        await cachedRaidHashesFile.delete();
-      }
-
-      if (await cachedProfileFile.exists()) {
-        await cachedProfileFile.delete();
-      }
-
       prefs.remove("latestMembership");
       prefs.remove("latestToken");
-      prefs.remove("littlelight_secret");
-      prefs.remove("littlelight_device_id");
     } catch (e) {
       print(e);
     }
