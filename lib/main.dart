425d3557ba9b5e5d2a1b29e9ce78854dbb766502
Yannis <yannis.b8@gmail.com>
2021-12-03 18:09:38 +0100

    fix json_token

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,18 @@ import 'package:quria/presentation/router.dart';
 import 'package:quria/presentation/var/routes.dart';
 
 void main() async {
+  StorageService.init();
   await dotenv.load(fileName: ".env");
   final AuthService auth = AuthService();
-  BungieNetToken token = {
-    'accessToken':
+  BungieNetToken token = BungieNetToken.fromJson({
+    'access_token':
         'CMraAxKGAgAg7dI+0TkMGh2uh5L3BF4tvx/zOmVisBiea6PBQf/lm4zgAAAAnCkNEwBf6Lsvtc6BbSSDmCObyW6vaLiISD453n7Fyy9nbQxiCwK5DqyxBCbsyjiEGe1OZBkiDKwQ345UObPGivziNHHOFaSo9tele6rSu8WqILkkfajnmABN5W3/VNHfkauDmocu4IPBa7ObnHwCQhJ3X0cshmisE+M5UqisIYy3ppOrfHqGnNxxsX5/P1LsjR0Vc8cd1W6eaDZoSAOHlMedKs/XkwIYPOUFcDQChqWk1JdxFaVJ/ycQCG641FDdnS6qfikNvgC6vwaZm6JkkxcrmBQMHNpcr40Z+IJ5oOo=',
-    'refreshToken':
+    'expires_in': 3600,
+    'refresh_token':
         'CMraAxKGAgAgQpzRp7QWUtRVShDD4ebDYWnMQVHHWl1dnZevpWwv6XPgAAAAyHZqrwzy3ymqmzQAKZY5kqYBEtfU1BPAJq19gbF486zsq+lWy3CfAwnra/Gpfg6TkjFonIaYpdbSFLdZ/ij+JWsiRefHapizhxjmztiCB2ML8mnhwMYu6Fnm0bOhkjvIq6kKuEkSR0b5Tk0UcvG/x+wPxWjQvBkakujHmzKaraUa+++wU/YuCP6ZuV9p60tXRS8s4PGKAV3RKHSppsC/pxYHs00lShOd/EJcA5jeCwC6pBFdbUv7mOuTaK4Nk/aQkCzuKJSI5GnawJCMw/VXkwHBAWwb9svUsIBOygOXO0o=',
-    'expiration': 3600,
-    'membershipId': '11319478',
-    'refreshTokenExpireIn': 3600,
-  } as BungieNetToken;
+    'refresh_expires_in': 3600,
+    'membership_id': '11319478',
+  });
   await auth.saveToken(token);
 
   runApp(QuriaApp(
@@ -33,7 +34,6 @@ class QuriaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    StorageService.init();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
