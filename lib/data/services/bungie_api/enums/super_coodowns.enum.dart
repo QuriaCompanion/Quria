d1ccf89f75e58b69452613ab2b95d1fcb5ec05a6
Pacski <43131953+pacski@users.noreply.github.com>
2022-02-17 13:09:24 +0100

    add super timer (#13)
    authored: Remy <remy@spliiit.com>

---
+++ lib/data/services/bungie_api/enums/super_coodowns.enum.dart
@@ -0,0 +1,89 @@
+class SuperCooldown {
+  static const Map<int, Map<int, int>> superMap = {
+    499823166: tier1, // Spectral
+    1757742244: tier1, // fist of havoc
+    1722642322: tier1, // hammer of sol
+    2021620139: tier1, // behemoth
+    194702279: tier2, // nova warp
+    368405360: tier2, //sentinel
+    4102085486: tier2, //daybreak
+    178252917: tier2, // stormtrance
+    3882393894: tier2, // chaos reach
+    675014898: tier2, // golden gun
+    3931765019: tier3, // shadowshot
+    3078181438: tier3, //arcbolt
+    1323416107: tier3, // burning maul
+    2936898795: tier3, //arc staff
+    3082407249: tier3, // nova bomb
+    2795355746: tier3, // thundercrash
+    2625980631: tier4, //Silence and squall
+    1590824323: tier4, // blade barrage
+    935376049: tier5, // well of radiance
+  };
+
+  static const Map<int, int> tier1 = {
+    0: 901,
+    1: 797,
+    2: 715,
+    3: 625,
+    4: 593,
+    5: 564,
+    6: 538,
+    7: 514,
+    8: 499,
+    9: 485,
+    10: 479
+  };
+  static const Map<int, int> tier2 = {
+    0: 801,
+    1: 709,
+    2: 636,
+    3: 556,
+    4: 527,
+    5: 501,
+    6: 478,
+    7: 457,
+    8: 444,
+    9: 431,
+    10: 425
+  };
+  static const Map<int, int> tier3 = {
+    0: 720,
+    1: 638,
+    2: 572,
+    3: 500,
+    4: 474,
+    5: 451,
+    6: 430,
+    7: 411,
+    8: 399,
+    9: 388,
+    10: 383
+  };
+  static const Map<int, int> tier4 = {
+    0: 655,
+    1: 580,
+    2: 520,
+    3: 455,
+    4: 431,
+    5: 410,
+    6: 391,
+    7: 374,
+    8: 363,
+    9: 353,
+    10: 348
+  };
+  static const Map<int, int> tier5 = {
+    0: 600,
+    1: 531,
+    2: 477,
+    3: 417,
+    4: 395,
+    5: 376,
+    6: 359,
+    7: 343,
+    8: 333,
+    9: 323,
+    10: 319
+  };
+}
