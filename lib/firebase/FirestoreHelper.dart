074fdea3af8aa8a5e7e1d3757732693a4db2dd91
Remy <remy@spliiit.com>
2022-03-22 09:33:44 +0100

    get membership id in firestorehelper

--- lib/firebase/FirestoreHelper.dart
+++ lib/firebase/FirestoreHelper.dart
@@ -1,20 +1,26 @@
 // ignore_for_file: file_names
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:quria/data/services/bungie_api/account.service.dart';
 import 'package:uuid/uuid.dart';
+
+import '../data/models/Build.dart';
 
 class FirestoreHelper {
   final builders = FirebaseFirestore.instance.collection("Builder");
 
+  get instance => null;
+
 // Build
   create({
-    required String? userId,
+    // required String? userId,
     required String? helmet,
     required String? gauntlet,
     required String? armor,
     required String? boots,
-  }) {
+  }) async {
     var uuid = const Uuid();
+    var userId = await AccountService().getCurrentMembershipId();
 
     String uid = uuid.v1();
     Map<String, dynamic> map = {
@@ -37,5 +43,10 @@ class FirestoreHelper {
 
   delete(String uid) {
     builders.doc(uid).delete();
+  }
+
+  Future<Future<QuerySnapshot<Map<String, dynamic>>>> list() async {
+    var userId = await AccountService().getCurrentMembershipId();
+    return builders.where(builders, isEqualTo: userId).get();
   }
 }
