4bbee6712fb233610db88f9ff85e7c51d7350a32
Yannis <yannis.b8@gmail.com>
2022-08-25 23:00:31 +0200

    build: :art: updated minSDKversion

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,14 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <isar_flutter_libs/isar_flutter_libs_plugin.h>
-#include <protocol_handler/protocol_handler_plugin.h>
+#include <uni_links_desktop/uni_links_desktop_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   IsarFlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
-  ProtocolHandlerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ProtocolHandlerPlugin"));
+  UniLinksDesktopPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UniLinksDesktopPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
